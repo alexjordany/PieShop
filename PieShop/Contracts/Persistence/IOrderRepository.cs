69ea9bb17f00671203fb021e1ff2bd46e601300f
Alex Jordany Parrales López <alexjordany@outlook.com>
2022-07-08 13:42:20 -0600

    Implementing Checkout

---
+++ PieShop/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using PieShop.Models;
+
+namespace PieShop.Contracts.Persistence;
+
+public interface IOrderRepository
+{
+    void CreateOrder(Order order);
+}
---
+++ PieShop/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using PieShop.Contracts.Persistence;
+using PieShop.Models;
+
+namespace PieShop.Persistence.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly PieShopDbContext _dbContext;
+    private readonly IShoppingCart _shoppingCart;
+
+    public OrderRepository(PieShopDbContext dbContext, IShoppingCart shoppingCart)
+    {
+        _dbContext = dbContext;
+        _shoppingCart = shoppingCart;
+    }
+
+    public void CreateOrder(Order order)
+    {
+        order.OrderPlaced = DateTime.Now;
+
+        List<ShoppingCartItem>? shoppingCartItems = _shoppingCart.ShoppingCartItems;
+        order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+
+        order.OrderDetails = new List<OrderDetail>();
+
+        foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+        {
+            var orderDetail = new OrderDetail
+            {
+                Amount = shoppingCartItem.Amount,
+                PieId = shoppingCartItem.Pie.PieId,
+                Price = shoppingCartItem.Pie.Price
+            };
+            order.OrderDetails.Add(orderDetail);
+        }
+
+        _dbContext.Orders.Add(order);
+        _dbContext.SaveChanges();
+    }
+}
